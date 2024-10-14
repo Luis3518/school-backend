bd511cf6ea72c8a5566ddb54abd6b8f0f0f890e7
Luis Miguel Rodriguez <48697966+Luis3518@users.noreply.github.com>
2024-10-13 21:36:58 -0300

    fix-api-response

--- app/Http/Controllers/AlumnoController.php
+++ app/Http/Controllers/AlumnoController.php
@@ -3,80 +3,89 @@
 namespace App\Http\Controllers;
 
 use App\Models\Alumno;
-use Illuminate\Http\Request;
 use App\Models\Curso;
-
+use Illuminate\Http\Request;
 
 class AlumnoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index(Request $request)
     {
         $cursoId = $request->input('curso');
         $cursos = Curso::all();
-    
+
         if ($cursoId) {
             $alumnos = Alumno::where('curso_id', $cursoId)->get();
         } else {
             $alumnos = Alumno::all();
         }
-    
+
+        if ($request->wantsJson()) {
+            return response()->json([
+                'alumnos' => $alumnos            ]);
+        }
+
         return view('alumnos.index', compact('alumnos', 'cursos'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+
     public function create()
     {
         $cursos = Curso::all();
         return view('alumnos.create', compact('cursos'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+
     public function store(Request $request)
     {
         $alumno = Alumno::create($request->all());
+
+        if ($request->wantsJson()) {
+            return response()->json($alumno, 201);
+        }
+
         return redirect()->route('alumnos.index');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Alumno $alumno)
+
+    public function show(Request $request, Alumno $alumno)
     {
         $alumno = Alumno::with('curso')->findOrFail($alumno->id);
+
+        if ($request->wantsJson()) {
+            return response()->json($alumno);
+        }
+
         return view('alumnos.show', compact('alumno'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Alumno $alumno)
     {
         $cursos = Curso::all();
         return view('alumnos.edit', compact('alumno', 'cursos'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+
     public function update(Request $request, Alumno $alumno)
     {
         $alumno->update($request->all());
+
+        if ($request->wantsJson()) {
+            return response()->json($alumno);
+        }
+
         return redirect()->route('alumnos.index');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Alumno $alumno)
+
+    public function destroy(Request $request, Alumno $alumno)
     {
         $alumno->delete();
+
+        if ($request->wantsJson()) {
+            return response()->json(null, 204);
+        }
+
         return redirect()->route('alumnos.index');
     }
 }
--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -7,63 +7,72 @@ use Illuminate\Http\Request;
 
 class CursoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+
+    public function index(Request $request)
     {
         $cursos = Curso::all();
+        
+        if ($request->wantsJson()) {
+            return response()->json($cursos);
+        }
+        
         return view('cursos.index', compact('cursos'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+
     public function create()
     {
         return view('cursos.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+
     public function store(Request $request)
     {
         $curso = Curso::create($request->all());
+        
+        if ($request->wantsJson()) {
+            return response()->json($curso, 201);
+        }
+        
         return redirect()->route('cursos.index');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Curso $curso)
+ 
+    public function show(Request $request, Curso $curso)
     {
+        if ($request->wantsJson()) {
+            return response()->json($curso);
+        }
+        
         return view('cursos.show', compact('curso'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Curso $curso)
     {
         return view('cursos.edit', compact('curso'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+
     public function update(Request $request, Curso $curso)
     {
         $curso->update($request->all());
+        
+        if ($request->wantsJson()) {
+            return response()->json($curso);
+        }
+        
         return redirect()->route('cursos.index');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Curso $curso)
+
+    public function destroy(Request $request, Curso $curso)
     {
         $curso->delete();
+        
+        if ($request->wantsJson()) {
+            return response()->json(null, 204);
+        }
+        
         return redirect()->route('cursos.index');
     }
 }
